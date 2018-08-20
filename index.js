fa1ad33e7c7b8297a2f25051e465df7819e9fd5b
Gabriel Morris <gabemorris@outlook.com>
2018-08-20 09:37:48 -0700

    Add year of birth function

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 "use strict";
 
 function createGreeting(name, age) {
-  const yearOfBirth = 2018 - age;
+  const yearOfBirth = getYearOfBirth(age);
   return `Hello, my name is ${name} and I am ${age}. I was born in ${yearOfBirth}`;
+}
+
+function getYearOfBirth(age) {
+  return 2018 - age;
 }
 
 const greeting = createGreeting("Gabriel", 23);
