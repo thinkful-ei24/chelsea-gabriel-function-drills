2c8d80f55972207bab96912f273c482ae452cf13
clkent <chelsea.kent@gmail.com>
2018-08-20 12:03:26 -0700

    decode problem

--- drill-two.js
+++ drill-two.js
@@ -17,13 +17,45 @@ function beyond(num) {
   }
 }
 
-console.log('positive');
-beyond(1);
-console.log('negative');
-beyond(-1);
-console.log('infinity');
-beyond(Infinity);
-console.log('0');
-beyond(0);
-console.log('negative infinity');
-beyond(Number.NEGATIVE_INFINITY);
+// console.log('positive');
+// beyond(1);
+// console.log('negative');
+// beyond(-1);
+// console.log('infinity');
+// beyond(Infinity);
+// console.log('0');
+// beyond(0);
+// console.log('negative infinity');
+// beyond(Number.NEGATIVE_INFINITY);
+
+
+// Cracking the code - take in string, split into array of individual letters, loop over array, check first character, decoded array, join into a string, log it  
+
+const codeString = "craft block argon meter bells brown croon droop";
+const codedStringArray = codeString.split(" ");
+const decodedArray = [];
+
+for (let i = 0; i < codedStringArray.length; i++) {
+   decodedArray.push(decode(codedStringArray[i]));
+}
+
+function decode(word){
+let decodedChar = "";
+
+    if (word.startsWith("a")){
+        decodedChar = word.substr(1,1);
+    } else if (word.startsWith("b")) {
+        decodedChar = word.substr(2,1);
+    } else if (word.startsWith("c")) {
+        decodedChar = word.substr(3,1);
+    } else if (word.startsWith("d")) {
+        decodedChar = word.substr(4,1);
+    } else {
+        decodedChar = " ";
+    }
+
+    return decodedChar;
+
+}
+
+console.log(decodedArray.join(""));
