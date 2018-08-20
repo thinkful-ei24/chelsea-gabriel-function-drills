799e2d7205cc506f5641d4d2acc38bac4c4e2a24
Gabriel Morris <gabemorris@outlook.com>
2018-08-20 10:57:24 -0700

    Rename index

--- drill-one.js
+++ drill-one.js
@@ -1,8 +1,11 @@
-"use strict";
+'use strict';
 
 function createGreeting(name, age) {
   if (!name || !age) {
-    throw new Error("Invalid arguments provided");
+    throw new Error('Invalid arguments provided');
+  }
+  if (typeof age !== 'number' || typeof name !== 'string') {
+    throw new TypeError();
   }
   const yearOfBirth = getYearOfBirth(age);
   return `Hello, my name is ${name} and I am ${age}. I was born in ${yearOfBirth}`;
@@ -10,13 +13,13 @@ function createGreeting(name, age) {
 
 function getYearOfBirth(age) {
   if (age < 0) {
-    throw new Error("Age cannot be negative");
+    throw new Error('Age cannot be negative');
   }
   return 2018 - age;
 }
 
 try {
-  const greeting = createGreeting("Gabriel", 23);
+  const greeting = createGreeting('Gabriel', 23);
   console.log(greeting);
 } catch (error) {
   console.error(error);
